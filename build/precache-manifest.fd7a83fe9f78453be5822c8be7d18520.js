97ac42ebc6b2c1850ab693527b68c73ce0ee78a9
Survival <bankomah63@gmail.com>
2019-12-23 19:08:30 +0000

    react files

---
+++ build/precache-manifest.fd7a83fe9f78453be5822c8be7d18520.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979f3203343fce3ae6152c2672f292e4",
+    "url": "/benji-lolo.github.io/jccc.git/index.html"
+  },
+  {
+    "revision": "295f670adc5d4a6f8de9",
+    "url": "/benji-lolo.github.io/jccc.git/static/css/main.27358cb5.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/benji-lolo.github.io/jccc.git/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "295f670adc5d4a6f8de9",
+    "url": "/benji-lolo.github.io/jccc.git/static/js/main.5ae04f02.chunk.js"
+  },
+  {
+    "revision": "0cf22c72cc575dd5e592",
+    "url": "/benji-lolo.github.io/jccc.git/static/js/runtime~main.ffde1df7.js"
+  }
+]);
